5fd6d99d4ef2c6ac65d50ca7eb93de6261f93143
王耀诚 <yocson@outlook.com>
2017-05-22 13:40:16 +0800

    增加404 500界面

--- ts.py
+++ ts.py
@@ -26,5 +26,14 @@ def poets():
 def poet():
     pass
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
