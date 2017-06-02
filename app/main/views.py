2cfc7158fd19cf257f44dd85d980d673c53a6f9b
王耀诚 <yocson@outlook.com>
2017-06-02 13:30:05 +0800

    新的程序结构

---
+++ app/main/views.py
@@ -0,0 +1,18 @@
+from flask import render_template, session, redirect, url_for
+
+from . import main
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/poems')
+def poems():
+    return render_template('poemlist.html')
+
+
+@main.route('/poems/<title>')
+def poem(title):
+    return render_template('poem.html', title=title)
